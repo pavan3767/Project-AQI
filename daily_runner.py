c0b81f8c22b925bedad8a912dd14abc6468c49ee
pavan3767 <66664441+pavan3767@users.noreply.github.com>
2025-12-05 01:05:59 +0530

    Update daily_runner.py

--- daily_runner.py
+++ daily_runner.py
@@ -7,7 +7,7 @@ from statsmodels.tsa.vector_ar.vecm import VECM
 from statsmodels.tsa.vector_ar.vecm import select_coint_rank
 
 # --- CONFIGURATION ---
-FILE_NAME = "Cleaned AQI Bulk data (2nd Dec).csv" # Your existing filename
+FILE_NAME = "Cleaned AQI Bulk data (4th Dec).csv" # Your existing filename
 FORECAST_FILE = "latest_forecast.csv"
 STATION_ID = 13738  # Perungudi
 TOKEN = os.environ.get("WAQI_TOKEN") # Securely loaded from secrets
@@ -105,4 +105,5 @@ forecast_df = pd.DataFrame({
 forecast_df.to_csv(FORECAST_FILE, index=False)
 
 print("Forecast generated and saved.")
+
 
